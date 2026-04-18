8b4a80c77d41a0235067353c8d45fa06fc330939
Felix Dombek <felics_dombek@hotmail.com>
2026-04-18 22:05:07 +0200

    Apply suggestion from @FelixDombek

--- tests/test_embedding_map.py
+++ tests/test_embedding_map.py
@@ -118,7 +118,9 @@ class TestBuildEmbeddingMap:
         from takeout_rater.clustering.embedding_map import build_embedding_map
 
         result = build_embedding_map([])
-        assert result == {"points": [], "clusters": [], "total": 0}
+        assert result == {"points": [], "clusters": [], "total": 0,  "params": {
+           "clustering_method": "kmeans",
+           "max_clusters": 24}}
 
     def test_single_point(self) -> None:
         from takeout_rater.clustering.embedding_map import build_embedding_map
